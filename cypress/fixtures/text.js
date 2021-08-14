6a410cfc4608b43d129eba949c980ab528bea2d6
Ghada Ayari <gayari@iobeya.com>
2021-08-14 23:33:50 +0100

    Add test data files

---
+++ cypress/fixtures/text.js
@@ -0,0 +1,3 @@
+export const text_lockedOutUserErrorMessage = 'Epic sadface: Sorry, this user has been locked out.';
+export const text_requiredUsernameErrorMessage = 'Epic sadface: Username is required';
+export const text_requiredPasswordErrorMessage = 'Epic sadface: Password is required';
