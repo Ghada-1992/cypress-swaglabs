e6336b034328489421d57dda149183634012e5df
Ghada Ayari <gayari@iobeya.com>
2021-08-14 23:53:40 +0100

    Update HomePage class

--- cypress/support/page-objects/home-page.js
+++ cypress/support/page-objects/home-page.js
@@ -2,34 +2,38 @@
 
 export class HomePage {
 
-    shoppingCart = '.shopping_cart_link';
-    inventoryList = '.inventory_list';
-    menuButton = '#react-burger-menu-btn';
-    menuItems = '.menu-item'
-    logoutLink = '#logout_sidebar_link';
-    aboutLink = '#about_sidebar_link';
+  shoppingCart = ".shopping_cart_link";
+  inventoryList = ".inventory_list";
+  menuButton = "#react-burger-menu-btn";
+  menuItems = ".menu-item";
+  logoutLink = "#logout_sidebar_link";
+  aboutLink = "#about_sidebar_link";
 
-    getMenuButton() {
-        return cy.get(this.menuButton);
-    }
-
-    clickMenuButton() {
-        this.getMenuButton().click();
-    }
-
-    clickLogoutLink() {
-        cy.get(this.logoutLink)
-            .click();
-    }
-
-    clickAboutLink() {
-        cy.get(this.aboutLink)
-            .click();
-    }
-
-    getMenuItems() {
-        return cy.get(this.menuItems);
-    }
-  
+  getShoppingCart() {
+    return cy.get(this.shoppingCart);
   }
-  
+
+  getInventoryList() {
+    return cy.get(this.inventoryList);
+  }
+
+  getMenuButton() {
+    return cy.get(this.menuButton);
+  }
+
+  clickMenuButton() {
+    this.getMenuButton().click();
+  }
+
+  clickLogoutLink() {
+    cy.get(this.logoutLink).click();
+  }
+
+  clickAboutLink() {
+    cy.get(this.aboutLink).click();
+  }
+
+  getMenuItems() {
+    return cy.get(this.menuItems);
+  }
+}
