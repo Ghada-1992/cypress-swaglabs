b409b1aee953c62f82d257325a748f6fc18c5571
Ghada Ayari <gayari@iobeya.com>
2021-08-14 23:41:50 +0100

    Add home test spec

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,16 +1,50 @@
-/// <reference types="cypress" />
+//// <reference types="cypress" />
 
-describe('Home Page Tests', () => {
+ const data = require("../fixtures/test-data.json");
+ const text = require("../fixtures/text.js");
 
-  beforeEach(()=>{
-  })
+ import { HomePage } from "../support/page-objects/home-page";
+ import { LoginPage } from "../support/page-objects/login-page";
 
-  it('Verify User can successfully logout from the application', () => {
+ describe("Home Page Tests", () => {
+   const loginPage = new LoginPage();
+   const homePage = new HomePage();
 
-  });
+   beforeEach(() => {
+     cy.visit("");
+     loginPage.getLoginLogo().should("be.visible");
+     loginPage.signIn(data.username, data.password);
+   });
 
-  it.only('Verify that the application redirects to the "About" page', () => {
+   it.only("Verify User can successfully logout from the application", () => {
+     homePage.getMenuButton().should("be.visible");
+     homePage.clickMenuButton();
+     for (let val in data.menuItems) {
+       homePage
+         .getMenuItems()
+         .eq(val)
+         .should("be.visible")
+         .and("have.text", data.menuItems[val]);
+     }
+     homePage.clickLogoutLink();
+     cy.url().should("eq", Cypress.config().baseUrl);
+     loginPage.getUsernameField().should("be.visible");
+     loginPage.getPasswordField().should("be.visible");
+     loginPage.getLoginButton().should("be.visible");
+   });
 
-  });
+   it.only('Verify that the application redirects to the "About" page', () => {
+     homePage.getMenuButton().should("be.visible");
+     homePage.clickMenuButton();
+     for (let val in data.menuItems) {
+       homePage
+         .getMenuItems()
+         .eq(val)
+         .should("be.visible")
+         .and("have.text", data.menuItems[val]);
+     }
+     homePage.clickAboutLink();
+     cy.url().should("eq", data.aboutRedirectionUrl);
+   });
 
-});
+ });
