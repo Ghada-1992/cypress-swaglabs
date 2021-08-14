bdf05a681f4593f5a2adf950cd6ca0a14d18f950
Ghada Ayari <gayari@iobeya.com>
2021-08-14 23:39:38 +0100

    Add login test spec

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,24 +1,55 @@
 /// <reference types="cypress" />
 
-describe('Login Functionality Tests', () => {
+const data = require("../fixtures/test-data.json");
+const text = require("../fixtures/text.js");
 
-  beforeEach(()=>{
-  })
+import { HomePage } from "../support/page-objects/home-page";
+import { LoginPage } from "../support/page-objects/login-page";
 
-  it('Verify that a locked user cannot login to the system', () => {
+describe("Login Functionality Tests", () => {
+  const loginPage = new LoginPage();
+  const homePage = new HomePage();
 
+  beforeEach(() => {
+    cy.visit("");
+    loginPage.getLoginLogo().should("be.visible");
   });
 
-  it('Verify User cannot login without a username', () => {
-
+  it("Verify that a locked user cannot login to the system", () => {
+    loginPage.enterUsername(data.lockedOutUsername);
+    loginPage.enterPassword(data.password);
+    loginPage.clickSubmit();
+    loginPage
+      .getErrorMessage()
+      .should("be.visible")
+      .and("have.text", text.text_lockedOutUserErrorMessage);
   });
 
-  it('Verify User cannot login without a password', () => {
-
+  it("Verify User cannot login without a username", () => {
+    loginPage.enterPassword(data.password);
+    loginPage.clickSubmit();
+    loginPage
+      .getErrorMessage()
+      .should("be.visible")
+      .and("have.text", text.text_requiredUsernameErrorMessage);
   });
-  
-  it('Verify User can login with correct credentials', () => {
 
+  it("Verify User cannot login without a password", () => {
+    loginPage.enterUsername(data.username);
+    loginPage.clickSubmit();
+    loginPage
+      .getErrorMessage()
+      .should("be.visible")
+      .and("have.text", text.text_requiredPasswordErrorMessage);
+  });
+
+  it("Verify User can login with correct credentials", () => {
+    loginPage.enterUsername(data.username);
+    loginPage.enterPassword(data.password);
+    loginPage.clickSubmit();
+    cy.url().should("eq", Cypress.config().baseUrl + data.inventoryPagePath);
+    homePage.getShoppingCart().should("be.visible");
+    homePage.getInventoryList().should("be.visible");
   });
 
 });
